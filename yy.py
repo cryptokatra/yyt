1b30141409e8f1efa07e1d7e15520c148e73ec0f
cryptokatra <129519216+cryptokatra@users.noreply.github.com>
2023-04-27 18:14:52 +0800

    Update yy.py

--- yy.py
+++ yy.py
@@ -1,31 +1,55 @@
 import streamlit as st
-import youtube_dl
+from pytube import YouTube
+import moviepy.editor as mp
 
-# 根据给定的URL下载视频
-def download_video(url):
+st.title("YouTube Downloader")
+
+# Function to download video
+def download_video(video_url):
     try:
-        with youtube_dl.YoutubeDL({'verbose': True}) as ydl:
-            info = ydl.extract_info(url, download=False)
-            formats = info.get('formats',None)
-            for f in formats:
-                if f.get('format_id',None) == '22':
-                    ydl.download([url])
-                    return True
-        return False
+        yt = YouTube(video_url)
+        st.write(f"Title: {yt.title}")
+        st.write(f"Number of views: {yt.views}")
+        st.write(f"Length of video: {yt.length} seconds")
+        stream = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
+        stream.download()
+        st.success("Video Downloaded Successfully")
     except Exception as e:
-        st.write("Error:", e)
-        return False
+        st.warning("Error: Could not download video")
+        st.write(e)
 
-# 主应用程序
+# Function to download audio
+def download_audio(video_url):
+    try:
+        yt = YouTube(video_url)
+        st.write(f"Title: {yt.title}")
+        st.write(f"Number of views: {yt.views}")
+        st.write(f"Length of audio: {yt.length} seconds")
+        audio = yt.streams.filter(only_audio=True).first()
+        audio.download()
+        audio_file = audio.default_filename
+        audio_clip = mp.AudioFileClip(audio_file)
+        audio_clip.write_audiofile(f"{yt.title}.mp3")
+        st.success("Audio Downloaded Successfully")
+    except Exception as e:
+        st.warning("Error: Could not download audio")
+        st.write(e)
+
+# Main function
 def main():
-    st.title("YouTube Downloader")
-    st.write("Enter the YouTube URL below to download the video")
-    url = st.text_input("URL")
+    st.sidebar.title("Select Download Type")
+    download_type = st.sidebar.selectbox("", ("Video", "Audio"))
+
+    st.subheader("Enter YouTube URL")
+    video_url = st.text_input("", "Paste the YouTube URL here...")
+
     if st.button("Download"):
-        if download_video(url):
-            st.write("Downloaded Successfully!")
-        else:
-            st.write("Download Failed! Please Try Again.")
+        if not video_url:
+            st.warning("Please enter a valid YouTube URL")
+        elif download_type == "Video":
+            download_video(video_url)
+        elif download_type == "Audio":
+            download_audio(video_url)
 
 if __name__ == "__main__":
     main()
